16e1a7ad91b42ffa1d82900ce928263ae2a1fe3d
Marc Villanueva <marcvillanueva@protonmail.com>
2019-07-26 18:58:30 +0800

    Scaffold make:auth

--- config/services.php
+++ config/services.php
@@ -35,7 +35,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User::class,
+        'model' => AuthServer\User::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
         'webhook' => [
